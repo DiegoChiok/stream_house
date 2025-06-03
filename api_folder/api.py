b0d806b33e6d09bcc235563c41199b1f390652ce
Diego <raiderz8923@gmail.com>
2025-06-03 01:50:21 -0700

    display of poster

--- api_folder/api.py
+++ api_folder/api.py
@@ -6,16 +6,36 @@ load_dotenv()
 
 API_KEY = os.getenv("WATCHMODE_API_KEY")
 
+BASE_POSTER_URL = "https://cdn.watchmode.com"  
+
 def get_trending_titles():
     url = f"https://api.watchmode.com/v1/list-titles/?apiKey={API_KEY}&types=movie&sort_by=popularity_desc&limit=10"
     response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        titles = data.get("titles", [])
-        return [{
-            "title": t.get("title"),
-            "poster": t.get("poster"),
-            "year": t.get("year")
-        } for t in titles]
-    else:
+    if response.status_code != 200:
         return []
+
+    data = response.json()
+    titles = data.get("titles", [])
+    movies = []
+
+    for t in titles:
+        title_id = t.get("id")
+        details_url = f"https://api.watchmode.com/v1/title/{title_id}/details/?apiKey={API_KEY}"
+        details_response = requests.get(details_url)
+
+        if details_response.status_code == 200:
+            details = details_response.json()
+            poster = details.get("poster", None)  
+            movies.append({
+                "title": t.get("title"),
+                "year": t.get("year"),
+                "poster": poster
+            })
+        else:
+            movies.append({
+                "title": t.get("title"),
+                "year": t.get("year"),
+                "poster": None
+            })
+
+    return movies
--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
-from api_folder.api import get_trending_titles  # from your API logic
+from api_folder.api import get_trending_titles  
 
-load_dotenv()  # Load environment variables from .env
-
-#app = Flask(__name__)
+load_dotenv()  
 app = Flask(__name__, template_folder='frontend')
 
+
+#@app.route('/')
+#def index():
+#    movies = get_trending_titles()
+#    return render_template('index.html', movies=movies)
 
 @app.route('/')
 def index():
     movies = get_trending_titles()
+    for m in movies:
+        print(m['poster'])
     return render_template('index.html', movies=movies)
 
 if __name__ == '__main__':
