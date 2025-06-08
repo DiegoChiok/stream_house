97ee46b013b2091efcfd6a862b12ce67c7d37068
Diego <raiderz8923@gmail.com>
2025-06-08 00:25:05 -0700

    working version with functional infinite scrol

--- api_folder/api.py
+++ api_folder/api.py
@@ -6,10 +6,8 @@ load_dotenv()
 
 API_KEY = os.getenv("WATCHMODE_API_KEY")
 
-BASE_POSTER_URL = "https://cdn.watchmode.com"  
-
-def get_trending_titles():
-    url = f"https://api.watchmode.com/v1/list-titles/?apiKey={API_KEY}&types=movie&sort_by=popularity_desc&limit=10"
+def get_trending_titles(page=1, genre_filter=None):
+    url = f"https://api.watchmode.com/v1/list-titles/?apiKey={API_KEY}&types=movie&sort_by=popularity_desc&limit=20&page={page}"
     response = requests.get(url)
     if response.status_code != 200:
         return []
@@ -23,19 +21,20 @@ def get_trending_titles():
         details_url = f"https://api.watchmode.com/v1/title/{title_id}/details/?apiKey={API_KEY}"
         details_response = requests.get(details_url)
 
-        if details_response.status_code == 200:
-            details = details_response.json()
-            poster = details.get("poster", None)  
-            movies.append({
-                "title": t.get("title"),
-                "year": t.get("year"),
-                "poster": poster
-            })
-        else:
-            movies.append({
-                "title": t.get("title"),
-                "year": t.get("year"),
-                "poster": None
-            })
+        if details_response.status_code != 200:
+            continue
+
+        details = details_response.json()
+        genres = details.get("genre_names", [])
+        poster = details.get("poster")
+
+        if genre_filter and genre_filter.lower() not in [g.lower() for g in genres]:
+            continue
+
+        movies.append({
+            "title": t.get("title"),
+            "year": t.get("year"),
+            "poster": poster
+        })
 
     return movies
--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-from flask import Flask, render_template
-from dotenv import load_dotenv
-import os
+from flask import Flask, render_template, jsonify, request
 from api_folder.api import get_trending_titles  
 
-load_dotenv()  
-app = Flask(__name__, template_folder='frontend')
-
-
-#@app.route('/')
-#def index():
-#    movies = get_trending_titles()
-#    return render_template('index.html', movies=movies)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    movies = get_trending_titles()
-    for m in movies:
-        print(m['poster'])
-    return render_template('index.html', movies=movies)
+    genre = request.args.get('genre')
+    movies = get_trending_titles(page=1, genre_filter=genre)
+    #print out ever genre checked and returned movies
+    print(f"Genre: {genre} | Movies Returned: {movies}") 
+    return render_template('index.html', movies=movies, genre=genre)
+
+
+@app.route('/load_more')
+def load_more():
+    page = int(request.args.get('page', 1))
+    genre = request.args.get('genre')
+    movies = get_trending_titles(page=page, genre_filter=genre)
+    return jsonify(movies)
+
+#@app.route('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
