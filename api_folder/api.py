6547b4aad666657037f9164cb2467c950f640591
Diego <raiderz8923@gmail.com>
2025-06-17 21:27:37 -0700

    change to list endpoint from manual filteration

--- api_folder/api.py
+++ api_folder/api.py
@@ -12,18 +12,18 @@ API_KEY = os.getenv("WATCHMODE_API_KEY")
 def get_sources():
     url = f'https://api.watchmode.com/v1/sources/?apiKey={API_KEY}'
     response = requests.get(url)
-
     #fail case: api call fails to get anything so return nothing
     if response.status_code != 200:
         return []
     
-    sources = response.json()
+    data = response.json()
+    #array of dict source with name and id
+    return [{"id": str(source["id"]), "name": source["name"]} for source in data]
 
-    return sources
     
 
 
-def get_trending_titles(page=1, genre_filter=None):
+#def get_trending_titles(page=1, genre_filter=None):
     #url for api to form get request
     url = f"https://api.watchmode.com/v1/list-titles/?apiKey={API_KEY}&types=movie&sort_by=popularity_desc&limit=20&page={page}"
     response = requests.get(url)
@@ -69,7 +69,7 @@ def get_trending_titles(page=1, genre_filter=None):
 
     return movies
 
-def get_service_titles(page=1, genre_filter=None):
+#def get_source_titles(page=1, source_filter=None):
     url = f"https://api.watchmode.com/v1/list-titles/?apiKey={API_KEY}&types=movie&sort_by=popularity_desc&limit=40&page={page}"
     response = requests.get(url)
 
@@ -83,18 +83,13 @@ def get_service_titles(page=1, genre_filter=None):
     titles = data.get("titles", [])
     movies = []
 
-
+###############################################################################################
     #loop through each movie title in list
     for t in titles:
         #get id, use id in url to collect info about movie
         title_id = t.get("id")
-        #use url to send request for more specific information on that title
-        details_url = f"https://api.watchmode.com/v1/title/{title_id}/details/?apiKey={API_KEY}"
-        details_response = requests.get(details_url)
 
-        #error code if title url gets nothing in return
-        if details_response.status_code != 200:
-            continue
+
 
         sources_url = f"https://api.watchmode.com/v1/title/{title_id}/sources/?apiKey={API_KEY}"
         sources_response = requests.get(sources_url)
@@ -103,16 +98,23 @@ def get_service_titles(page=1, genre_filter=None):
         if sources_response.status_code != 200:
             continue
 
-        #for each movie store response og poster and genre
-        details = details_response.json()
-        genres = details.get("genre_names", [])
-        poster = details.get("poster")
+        sources = sources_response.json()
+        #source_names = sources.get("name", [])
+        source_names = [s.get("name") for s in sources]
 
-        #checks genre filter to the genre of movie
-        if genre_filter and genre_filter.lower() not in [g.lower() for g in genres]:
+        if source_filter:
+            lower_source_names = [s.lower() for s in source_names if s]
+            if not any(sel.lower() in lower_source_names for sel in source_filter):
+                continue
+
+        details_url = f"https://api.watchmode.com/v1/title/{title_id}/details/?apiKey={API_KEY}"
+        details_response = requests.get(details_url)
+        if details_response.status_code != 200:
             continue
 
-        #movies list with title, year, and poster returned
+        details = details_response.json()
+        poster = details.get("poster")
+
         movies.append({
             "title": t.get("title"),
             "year": t.get("year"),
@@ -120,3 +122,48 @@ def get_service_titles(page=1, genre_filter=None):
         })
 
     return movies
+
+def get_source_titles_new(source_ids, page=1):
+    url = f"https://api.watchmode.com/v1/list-titles/"
+    #url = f'https://api.watchmode.com/v1/list-titles/?apiKey={API_KEY}&source_ids={source_ids}'
+    params = {
+        "apiKey": API_KEY,
+        "types": "movie",  # or "tv_series" etc.
+        "source_ids": ",".join(source_ids),  # IDs as string
+        "sort_by": "popularity_desc",
+        "page": page,
+        "limit": 40  # or up to 250
+    }
+
+    #response = requests.get(url)
+    response = requests.get(url, params=params)
+    if response.status_code != 200:
+        return []
+
+    data = response.json()
+    titles = data.get("titles", [])
+    movies = []
+
+    #loop through each movie title in list
+    for t in titles:
+        #get id, use id in url to collect info about movie
+        title_id = t.get("id")
+
+        details_url = f"https://api.watchmode.com/v1/title/{title_id}/details/?apiKey={API_KEY}"
+        details_response = requests.get(details_url)
+        if details_response.status_code != 200:
+            continue
+
+        details = details_response.json()
+        poster = details.get("poster")
+
+        movies.append({
+            "title": t.get("title"),
+            "year": t.get("year"),
+            "poster": poster
+        })
+
+    return movies
+
+
+    #return data.get("titles", [])
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-from api_folder.api import get_trending_titles, get_sources
+from api_folder.api import get_trending_titles, get_sources, get_source_titles_new
 
 app = Flask(__name__)
 
@@ -13,9 +13,10 @@ def index():
     #return render_template('index.html', movies=movies, genre=genre)
 ########################################################################################
     sources = get_sources()
-    #movies = get_trending_titles(page=1, sources_filter=sources)
-    #return render_template('index.html', movies-movies, sources=sources)
-    return render_template('index.html', sources=sources)
+    selected_sources = request.args.getlist('sources')  
+    #movies = get_source_titles(page=1, source_filter=selected_sources)  
+    movies = get_source_titles_new(source_ids=selected_sources, page=1)
+    return render_template('index.html', movies=movies, sources=sources, selected_sources=selected_sources)
 
 
 @app.route('/load_more')
@@ -31,3 +32,16 @@ def load_more():
 #fixed app run
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+    #<form method="get" action="/">
+    #    <label for="source">Filter by Streaming Availability:</label><br>
+    #    {% for source in sources %}
+    #        <input type="checkbox" name="sources" value="{{ source.name }}"
+    #            {% if source.name in selected_sources %}checked{% endif %}>
+    #        {{ source.name }}<br>
+    #    {% endfor %}
+    #    <button type="submit">Apply</button>
+    #</form>
